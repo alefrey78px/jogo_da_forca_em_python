14aec5c90af5e25478d0845f07f1f8a37d6dfae1
Alexandre Frey <ale.bsd@gmail.com>
2024-07-28 18:49:30 -0300

    alterando nomes atributos para melhorar a clareza da sua função

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 
 class Palavra:
     def __init__(self):
-        self.palavra = ""
+        self.palavra_selecionada = ""
         self.dica = ""
 
     def sortear_palavra(self):
@@ -18,15 +18,15 @@ class Palavra:
             quit()
 
         palavra_com_dica = random.choice(lista_de_palavras).upper().split(':')
-        self.palavra = palavra_com_dica[0]
+        self.palavra_selecionada = palavra_com_dica[0]
         self.dica = palavra_com_dica[1]
 
 
 class Jogador:
     def __init__(self, nome):
         self.nome = nome.upper()
-        self.chutes_corretos = ""
-        self.chutes_incorretos = ""
+        self.letras_corretas = ""
+        self.letras_incorretas = ""
         self.rodadas = 0
 
     def pedir_chute(self):
@@ -35,122 +35,122 @@ class Jogador:
             if len(chute) != 1:
                 print('INFORME APENAS UMA LETRA!')
                 continue
-            if chute in self.chutes_corretos or chute in self.chutes_incorretos:
+            if chute in self.letras_corretas or chute in self.letras_incorretas:
                 print(f'{chute}: ESSA LETRA JA FOI CHUTADA! TENTE OUTRA LETRA!')
                 continue
             return chute
 
 
-def animacao_enforcamento(numero_erros):
-    estagios = [
-        '''
-        |==========|
-        |          +
-        |
-        |
-        |
-        |
-        |
-        |________________________
-        ''',
-        '''
-        |==========|
-        |          +
-        |        (```)
-        |
-        |
-        |
-        |
-        |________________________
-        ''',
-        '''
-        |==========|
-        |          +
-        |        (```)
-        |          |
-        |
-        |
-        |
-        |________________________
-        ''',
-        '''
-        |==========|
-        |          +
-        |        (```)
-        |          |
-        |         /
-        |
-        |
-        |________________________
-        ''',
-        '''
-        |==========|
-        |          +
-        |        (```)
-        |          |
-        |         / \\
-        |
-        |
-        |________________________
-        ''',
-        '''
-        |==========|
-        |          +
-        |        (```)
-        |          |
-        |         / \\
-        |          |
-        |
-        |________________________
-        ''',
-        '''
-        |==========|
-        |          +
-        |        (```)
-        |          |
-        |         / \\
-        |          |
-        |         /
-        |________________________
-        ''',
-        '''
-        |==========|
-        |          +
-        |        (```)
-        |          |
-        |         / \\
-        |          |
-        |         / \\
-        |________________________
-        '''
-    ]
-    print(estagios[numero_erros])
-
-
 class Forca:
-    def __init__(self, jogador, palavra_sorteada):
-        self.jogador = jogador
-        self.palavra_sorteada = palavra_sorteada
-        self.palavra_oculta = ["_" if letra != "-" else "-" for letra in palavra_sorteada.palavra]
+    def __init__(self, jogador_atual, palavra_atual):
+        self.jogador = jogador_atual
+        self.palavra = palavra_atual
+        self.palavra_oculta = ["_" if letra != "-" else "-" for letra in palavra_atual.palavra_selecionada]
         self.chances = 7
 
     def marca_letras_corretas(self, chute):
-        for indice, letra in enumerate(self.palavra_sorteada.palavra):
+        for indice, letra in enumerate(self.palavra.palavra_selecionada):
             if letra == chute:
                 self.palavra_oculta[indice] = chute
 
-    def verifica_letras_faltantes(self):
+    def conta_letras_faltantes(self):
         return self.palavra_oculta.count("_")
 
     def verifica_letras_chutadas(self, chute):
-        if chute in self.palavra_sorteada.palavra:
-            self.jogador.chutes_corretos += chute
+        if chute in self.palavra.palavra_selecionada:
+            self.jogador.letras_corretas += chute
             return True
         else:
-            self.jogador.chutes_incorretos += chute
+            self.jogador.letras_incorretas += chute
             return False
 
-    def encerramento(self, venceu):
+    @staticmethod
+    def mostrar_animacao_enforcamento(numero_erros):
+        estagios = [
+            r'''
+            |==========|
+            |          +
+            |
+            |
+            |
+            |
+            |
+            |________________________
+            ''',
+            r'''
+            |==========|
+            |          +
+            |        (```)
+            |
+            |
+            |
+            |
+            |________________________
+            ''',
+            r'''
+            |==========|
+            |          +
+            |        (```)
+            |          |
+            |
+            |
+            |
+            |________________________
+            ''',
+            r'''
+            |==========|
+            |          +
+            |        (```)
+            |          |
+            |         /
+            |
+            |
+            |________________________
+            ''',
+            r'''
+            |==========|
+            |          +
+            |        (```)
+            |          |
+            |         / \
+            |
+            |
+            |________________________
+            ''',
+            r'''
+            |==========|
+            |          +
+            |        (```)
+            |          |
+            |         / \
+            |          |
+            |
+            |________________________
+            ''',
+            r'''
+            |==========|
+            |          +
+            |        (```)
+            |          |
+            |         / \
+            |          |
+            |         /
+            |________________________
+            ''',
+            r'''
+            |==========|
+            |          +
+            |        (```)
+            |          |
+            |         / \
+            |          |
+            |         / \
+            |________________________
+            '''
+        ]
+        print(estagios[numero_erros])
+
+    def mostrar_encerramento(self, venceu):
         if venceu:
             print(r'''
              _________________
@@ -168,7 +168,7 @@ class Forca:
              |_______________|
                   ''')
 
-            print(f'PARABENS {self.jogador.nome}! VOCE ACERTOU! A PALAVRA ERA: {self.palavra_sorteada.palavra}\n')
+            print(f'PARABENS {self.jogador.nome}! VOCE ACERTOU! A PALAVRA ERA: {self.palavra.palavra_selecionada}\n')
         else:
             print(r'''
                           _____
@@ -190,13 +190,14 @@ class Forca:
             ''')
 
             print(
-                f'SINTO MUITO {self.jogador.nome}, VOCE FOI ENFORCADO! A PALAVRA ERA {self.palavra_sorteada.palavra}.\n')
+                f'SINTO MUITO {self.jogador.nome},'
+                f' VOCE FOI ENFORCADO! A PALAVRA ERA {self.palavra.palavra_selecionada}.\n')
 
-    def apresentacao(self):
-        print(f'A PALAVRA TEM {len(self.palavra_oculta)} LETRAS. DICA: {self.palavra_sorteada.dica}')
-        self.formata_palavra_oculta_para_exibicao()
+    def mostrar_apresentacao(self):
+        print(f'A PALAVRA TEM {len(self.palavra_oculta)} LETRAS. DICA: {self.palavra.dica}')
+        self.mostrar_palavra()
 
-    def formata_palavra_oculta_para_exibicao(self):
+    def mostrar_palavra(self):
         print('\n')
         for letra in self.palavra_oculta:
             print('\t', letra, end='')
@@ -204,30 +205,30 @@ class Forca:
 
     def estatisticas(self):
         print('ESTATISTICAS:')
-        print(f'CHUTES CORRETOS..: {self.jogador.chutes_corretos}')
-        print(f'CHUTES INCORRETOS: {self.jogador.chutes_incorretos}')
+        print(f'CHUTES CORRETOS..: {self.jogador.letras_corretas}')
+        print(f'CHUTES INCORRETOS: {self.jogador.letras_incorretas}')
         print(f'NUMERO DE RODADAS: {self.jogador.rodadas}')
 
     def jogar(self):
-        self.apresentacao()
+        self.mostrar_apresentacao()
 
-        while self.chances > 0 and self.verifica_letras_faltantes() > 0:
+        while self.chances > 0 and self.conta_letras_faltantes() > 0:
             self.jogador.rodadas += 1
             chute = self.jogador.pedir_chute()
             self.marca_letras_corretas(chute)
-            self.formata_palavra_oculta_para_exibicao()
+            self.mostrar_palavra()
 
             if self.verifica_letras_chutadas(chute):
-                if self.verifica_letras_faltantes() == 0:
-                    self.encerramento(True)
+                if self.conta_letras_faltantes() == 0:
+                    self.mostrar_encerramento(True)
                     break
             else:
                 self.chances -= 1
-                animacao_enforcamento(len(self.jogador.chutes_incorretos))
+                self.mostrar_animacao_enforcamento(len(self.jogador.letras_incorretas))
                 print(f'NAO TEM A LETRA {chute}! VOCE TEM MAIS {self.chances} CHANCES!')
 
             if self.chances == 0:
-                self.encerramento(False)
+                self.mostrar_encerramento(False)
 
         self.estatisticas()
 
@@ -247,11 +248,19 @@ while True:
         print('SAINDO DO JOGO...')
         break
     elif opcao.lower() == 'j':
+        # pede o nome do jogador
         nome_jogador = input('DIGITE SEU NOME: ')
+
+        # instancia a classe Jogador passando o nome digitado
         jogador = Jogador(nome_jogador)
-        palavra_sorteada = Palavra()
-        palavra_sorteada.sortear_palavra()
-        jogo = Forca(jogador, palavra_sorteada)
+
+        # instancia a classe Palavra e aciona o metodo que sorteia a palavra
+        palavra = Palavra()
+        palavra.sortear_palavra()
+
+        # instancia a classe Forca passando nome do jogador, palavra sorteada
+        # e aciona o metodo jogar da mesma classe que contem a mecanica do jogo
+        jogo = Forca(jogador, palavra)
         jogo.jogar()
     else:
         print('SELECIONE UMA OPCAO VALIDA, J PARA JOGAR OU S PARA SAIR!')
